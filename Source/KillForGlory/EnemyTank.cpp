4d5b3721bdaa40deaf825fa20955f93facfec911
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2020-11-09 16:31:56 +0100

    Create enemy tank

---
+++ Source/KillForGlory/EnemyTank.cpp
@@ -0,0 +1,111 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyTank.h"
+
+#include "Components/BoxComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "KFGPlayer.h"
+#include "Engine/Engine.h"
+
+AEnemyTank::AEnemyTank()
+{
+    attackHitBox = CreateDefaultSubobject<UBoxComponent>("basicAttackHitBox");
+    attackHitBox->SetupAttachment(RootComponent);
+    
+    specialHitBox = CreateDefaultSubobject<UBoxComponent>("specialAttackHitBox");
+    specialHitBox->SetupAttachment(RootComponent);
+}
+
+void AEnemyTank::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    attackHitBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemyTank::OnAttackHitBoxBeginOverlap);
+    specialHitBox->OnComponentBeginOverlap.AddDynamic(this, &AEnemyTank::OnSpecialHitBoxBeginOverlap);
+
+    attackHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    specialHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AEnemyTank::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (isFollowingPlayer)
+    {
+        timeAttack += DeltaTime;
+        currentSpecialCooldown -= DeltaTime;
+    }
+    
+    if(isAttacking)
+    {
+        if(currentSpecialCooldown <= 0)
+        {
+            PlayAnimMontage(attackSpe);
+            currentSpecialCooldown = specialCooldown;
+        }
+        else
+            PlayAnimMontage(attackBasic);
+
+        isAttacking = false;
+    }
+}
+
+void AEnemyTank::enableBasicAttack()
+{
+    attackHitBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void AEnemyTank::disableBasicAttack()
+{
+    attackHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    isAttacking = false;
+}
+
+
+void AEnemyTank::OnAttackHitBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Blue, "Basic tank");
+    if(OtherActor->ActorHasTag("Player") && OtherComp->ComponentHasTag("Body"))
+    {
+        AKFGPlayer* player = Cast<AKFGPlayer>(OtherActor);
+        if(player != nullptr)
+            player->PlayerDamage(damage);
+    }
+}
+
+void AEnemyTank::startSpecialAttack()
+{
+    GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_Flying);
+}
+
+void AEnemyTank::stopSpecialAttack()
+{
+    GetCharacterMovement()->SetMovementMode(EMovementMode::MOVE_None);
+    isAttacking = false;
+}
+
+void AEnemyTank::enableSpecialAttack()
+{
+    specialHitBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void AEnemyTank::disableSpecialAttack()
+{
+    specialHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);    
+}
+
+void AEnemyTank::OnSpecialHitBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                             UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if(OtherActor->ActorHasTag("Player") && OtherComp->ComponentHasTag("Body"))
+    {
+        GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Blue, "Special tank");
+        
+        AKFGPlayer* player = Cast<AKFGPlayer>(OtherActor);
+        if(player != nullptr)
+            player->PlayerDamage(specialDamage);
+    }
+}
---
+++ Source/KillForGlory/EnemyTank.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "EnemyTank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KILLFORGLORY_API AEnemyTank : public AEnemy
+{
+	GENERATED_BODY()
+public:
+
+	AEnemyTank();
+	
+protected:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	//// Colliders
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category=Collision)
+	UBoxComponent* attackHitBox;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category=Collision)
+	UBoxComponent* specialHitBox;
+	////
+
+	//// Event
+	UFUNCTION()
+    void OnAttackHitBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                            int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+    void OnSpecialHitBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                            int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void enableBasicAttack();
+	UFUNCTION(BlueprintCallable)
+	void disableBasicAttack();
+
+	UFUNCTION(BlueprintCallable)
+	void startSpecialAttack();
+	UFUNCTION(BlueprintCallable)
+	void stopSpecialAttack();
+	UFUNCTION(BlueprintCallable)
+	void enableSpecialAttack();
+	UFUNCTION(BlueprintCallable)
+	void disableSpecialAttack();
+	////
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category=EnemyTank)
+	float specialCooldown = 0;
+	float currentSpecialCooldown = 0;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category=EnemyTank)
+	float specialDamage = 0;
+};
